5279687e497ff8da2fd45166274333f1706d34b1
Anatoly Karpovich <anatoly.karpovich90@gmail.com>
2025-05-15 22:27:16 +0200

    Lection 26

--- src/fixtures/mock.fixture.ts
+++ src/fixtures/mock.fixture.ts
@@ -1,4 +1,5 @@
-import { Page, test as base } from "@playwright/test";
+import { test as base } from "fixtures/api-services.fixture";
+import { Page } from "@playwright/test";
 import { apiConfig } from "config/api-config";
 import { STATUS_CODES } from "data/statusCodes";
 import { ICustomerResponse, ICustomersResponse } from "types/customer.types";
