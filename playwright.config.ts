1b266d49d5715c09bbea1d12db0213840e388aa8
Anatoly Karpovich <anatoly.karpovich90@gmail.com>
2025-05-27 21:21:17 +0200

    added docker to repo and ui-docker.yml

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 3 : 5,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [["html"], ["list"], ["allure-playwright"]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -65,7 +65,7 @@ export default defineConfig({
         storageState: "src/.auth/user.json",
       },
       dependencies: ["setup"],
-      testDir: "./src/ui/tests/SalesPortal/customers/e2e", //to speed up CI runs",
+      testDir: "./src/ui/tests/SalesPortal", //to speed up CI runs",
     },
 
     {
--- src/config/global.setup.ts
+++ src/config/global.setup.ts
@@ -2,8 +2,7 @@ import path from "path";
 import { rimraf } from "rimraf";
 
 export default function globalSetup() {
+  if (process.env.CI) return;
   const resultsPath = path.resolve(__dirname, "allure-results");
   rimraf.sync(resultsPath);
-
-  console.log("from global setup");
 }
